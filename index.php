624f5d5bb0e3c056408c725a760ef4920df4f285
zjonn <zjonnn@gmail.com>
2019-10-13 21:55:48 +0200

    Added max age

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     </head>
     <body>
     Kurs WWW 2019<br>
-        <?php 
+        <?php
+        header('Cache-Control: max-age=86400'); 
         print_r($_REQUEST, FALSE);
         echo "<br>";
         print_r($_GET, FALSE);
