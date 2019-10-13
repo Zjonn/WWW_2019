c7de7f50812965d1e66f38a685a0bb9fa5b2cae6
zjonn <zjonnn@gmail.com>
2019-10-13 16:49:02 +0200

    Added more prints

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo $_REQUEST["test"]; ?>
+<html>
+    <head>
+        <title>Kurs www</title>
+    </head>
+    <body>
+    Kurs WWW 2019
+        <?php 
+        print_r($_REQUEST["test"], FALSE);
+        print_r($_GET, FALSE);
+        print_r($_POST, FALSE);
+        ?>
+    </body>
+</html>
