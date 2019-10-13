63e7e1942d0a679d8314e61e2e422d7fc432fc90
zjonn <zjonnn@gmail.com>
2019-10-13 16:51:53 +0200

    Fixed request pirinting

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
         <title>Kurs www</title>
     </head>
     <body>
-    Kurs WWW 2019
+    Kurs WWW 2019<br>
         <?php 
-        print_r($_REQUEST["test"], FALSE);
+        print_r($_REQUEST, FALSE);
         print_r($_GET, FALSE);
         print_r($_POST, FALSE);
         ?>
