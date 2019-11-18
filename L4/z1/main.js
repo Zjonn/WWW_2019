f2180aceffef9bad92908b0db1546aff07573279
zjonn <zjonnn@gmail.com>
2019-11-18 01:46:38 +0100

    l4_1

--- L4/z1/main.js
+++ L4/z1/main.js
@@ -1,4 +1,17 @@
-document.getElementById('button').onclick =
+document.getElementById('f1').onblur =
     function () {
-        document.getElementById('test').innerHTML = '<b>ALA MA KOTA</b>';
+        var acc_num = document.getElementById('f1')
+        if (!acc_num.value.match("\d{16}")) {
+            alert("Incorrect value")
+        }
+
+    }
+
+    document.getElementById('f2').onblur =
+    function () {
+        var acc_num = document.getElementById('f2')
+        if (!acc_num.value.match("\d{11}")) {
+            alert("Incorrect value")
+        }
+
     }
