684ad2ad79a44b0b80d7e3469ef265cf20d061f4
zjonn <zjonnn@gmail.com>
2019-12-02 03:17:54 +0100

    Midway

--- L6/z3/main.js
+++ L6/z3/main.js
@@ -1,49 +1,18 @@
-var blocks = document.getElementsByName("b");
-var actual_block_idx = 0
-var actual_block_pos = 0
 
-let size = blocks.length
+$(document).ready(function () {
 
-let max_y = 50
-let step = 3
-var dir = 1
-
-function update_pos() {
-    if (dir == 1 && actual_block_pos >= max_y) {
-        actual_block_idx += 1
-        actual_block_pos = 0
-        if (actual_block_idx == size) {
-            actual_block_idx = 0
-            actual_block_pos = 50
-            dir = -1
+    $('#dialog-confirm').dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true,
+        buttons: {
+            "Delete all items": function () {
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                $(this).dialog("close");
+            }
         }
-    } else if (dir == -1 && actual_block_pos <= 0) {
-        actual_block_idx += 1
-        actual_block_pos = max_y
-        if (actual_block_idx == size) {
-            actual_block_idx = 0
-            actual_block_pos = 0
-            dir = 1
-        }
-    }
-    actual_block_pos += step * dir
-
-    let actual_block = blocks[actual_block_idx]
-    actual_block.style.top = actual_block_pos + "px"
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    blocks.forEach(element => {
-        element.style.backgroundColor = getRandomColor()
     });
-    setInterval(update_pos, 10);
-});
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+})
