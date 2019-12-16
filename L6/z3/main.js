d9b15ea57f8e89a851af0a6f4fdde441c9f0d7a9
Zjonn <zjonnn@gmail.com>
2019-12-16 15:45:55 +0100

    Fast deploy

--- L6/z3/main.js
+++ L6/z3/main.js
@@ -1,18 +1,68 @@
+var form;
 
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#create-user").click(function() {
+    dial_trigger_create();
+  });
 
-    $('#dialog-confirm').dialog({
-        resizable: false,
-        height: "auto",
-        width: 400,
-        modal: true,
-        buttons: {
-            "Delete all items": function () {
-                $(this).dialog("close");
-            },
-            Cancel: function () {
-                $(this).dialog("close");
-            }
-        }
-    });
-})
+  $("#rm_button").click(function() {
+    dial_trigger_del($(this));
+  });
+});
+
+function dial_trigger_del(context) {
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height: "auto",
+    width: 200,
+    modal: true,
+    buttons: {
+      "Delete row": function() {
+        context.closest("tr").remove();
+        $(this).dialog("close");
+      },
+      Cancel: function() {
+        $(this).dialog("close");
+      }
+    }
+  });
+}
+
+function dial_trigger_create() {
+  dialog = $("#dialog-form").dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+    //   "Create an account": create_user,
+      Cancel: function() {
+        $(this).dialog("close");
+      }
+    },
+    close: function() {
+      form[0].reset();
+    }
+  });
+}
+
+function create_user() {
+
+  $("#users > tbody:last-child").append(
+    "<tr>" +
+      "<td>" +
+      user[0] +
+      "</td>" +
+      "<td>" +
+      user[1] +
+      "</td>" +
+      "<td>" +
+      user[2] +
+      "</td>" +
+      "<td>" +
+      user[3] +
+      "</td>" +
+      '<td><button class="rm_button">Remove</button></td>' +
+      "</tr>"
+  );
+}
--- L8/z4/index.php
+++ L8/z4/index.php
@@ -1,23 +1,31 @@
 <?php
-    header('Cache-Control: max-age=86400'); 
-?>
-<html>
-    <head>
-        <title>Kurs www</title>
-    </head>
-    <body>
-    Kurs WWW 2019<br>
-        <?php
-        
-        print_r($_REQUEST, FALSE);
-        echo "<br>";
-        print_r($_GET, FALSE);
-        echo "<br>";
-        print_r($_POST, FALSE);
-        echo "<br>";
-        echo date("h:i") . "\r\n";
-        echo "<br>";
-        print_r($_SERVER, FALSE);
-        ?>
-    </body>
-</html>
+require "../bootstrap.php";
+use pet_controller;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+
+// all of our endpoints start with /person
+// everything else results in a 404 Not Found
+if ($uri[1] !== 'person') {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+
+// the user id is, of course, optional and must be a number:
+$userId = null;
+if (isset($uri[2])) {
+    $userId = (int) $uri[2];
+}
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+// pass the request method and user ID to the PersonController and process the HTTP request:
+$controller = new PersonController($dbConnection, $requestMethod, $userId);
+$controller->processRequest();
